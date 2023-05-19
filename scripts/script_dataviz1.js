4c83c92f89b880e5592ece9808240f1fdccbacc1
sol <solchuvaleggia@gmail.com>
2023-05-19 14:21:48 -0300

    dataviz 1 por mesas sin separar categorias

--- scripts/script_dataviz1.js
+++ scripts/script_dataviz1.js
@@ -1,53 +1,47 @@
-d3.csv('data/todos.csv', d3.autoType).then(data => {
-  const domiciliosFiltrados = ['LIMPIEZA Y RECOLECCIÓN', 'TRÁNSITO', 'CALLES Y VEREDAS', 'ARBOLADO Y ESPACIOS VERDES','TRÁMITES Y SERVICIOS','ORDENAMIENTO DEL ESPACIO PÚBLICO','MEDIOS DE TRANSPORTE','ALUMBRADO', 'RECICLADO Y PROTECCIÓN AMBIENTAL','CONTROL EDILICIO','FISCALIZACIÓN ACTIVIDADES COMERCIALES','BARRIOS EMERGENTES'];
-  dataFiltrada = data.filter(d => domiciliosFiltrados.includes(d.categoria));
-  let chart_def_1 = Plot.plot({
-    style:{
-      fontSize: 10,
-    },
-    width: 650, // Increase the chart width
-    height: 500, // Increase the chart height
-    font: 'Poppins', // Set font family to Poppins
-    marginLeft: 250,
-    marks: [
-      Plot.barX(
-        dataFiltrada,
-        Plot.groupY(
-          { x: "count" }, 
-          {
-            y: "categoria",
-            sort: { y: "x", reverse: true },  
-            fill: d => d.categoria === "LIMPIEZA Y RECOLECCIÓN" ? "#225EA8" : "#C7D9F1"        
-          } 
-        ), 
-      ),
-      Plot.text(
-        dataFiltrada,
-        Plot.groupY(
-          { x: "count", text:"count" }, 
-          {
-            y: "categoria",
-            //fill: d => d.categoria === "LIMPIEZA Y RECOLECCIÓN" ? "#225EA8" : "#C7D9F1"        ,
-            sort: { y: "x", reverse: true },  
-            dx: 30,
-            fontSize: 15,
-            fontWeight: 'bold'
-          } 
-        ), 
-      )
-    ],
-    y: {
-      label: "",
-      tickFormat: d => d.toLowerCase().replace(/\b\w/g, l => l.toUpperCase())
-        },
-    x: {
-      domain: [0, 4500],
-      label: "CANTIDAD DE CONTACTOS",
-      ticks: 0,
-    },
+d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(locale => {
+  d3.timeFormatDefaultLocale(locale);
 
+  d3.dsv(';', 'data/merge.csv', d3.autoType).then(data => {
+    console.log(data.length);
+    let chart_def_1 = Plot.plot({
+      x: {
+        type: 'time',
+        // Ver formatos https://github.com/d3/d3-time-format
+        label: 'Todo el año'
+      },
+      y: {
+        grid: true,
+        label: 'Cantidad de reclamos',
+        tickFormat: 'd',
+      },
+      marks: [
+        Plot.rectY(
+          data,
+          Plot.binX(
+            {
+              y: 'count',
+              title: d => JSON.stringify(d),
+            },
+            {
+              // https://github.com/d3/d3-time-format
+              // Convierte a un date JS
+              x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso),
+              // https://github.com/d3/d3-time#timeMonth
+              // Agrupamos en intervalo de meses
+              thresholds: d3.timeMonth,
+              // Agrupar por categoría
+              groupBy: d => d.categoria,
+            },
+          ),
+        ),
+      ],
+      color: {
+        scheme: 'YlGnBu',
+        legend: true,
+  
+      },
+    });
+    // Agregamos chart al div#chart de index.html
+    d3.select('#chart_def_1').append(() => chart_def_1);
   });
-
-  d3.select('#chart_def_1').append(() => chart_def_1);
-
 });
