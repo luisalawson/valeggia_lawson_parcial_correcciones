b72968ff1bfc64b6215ec693b0168d2e747ead00
luisalawson <79951357+luisalawson@users.noreply.github.com>
2023-05-24 09:58:54 -0300

    Update script_dataviz3.js

--- scripts/script_dataviz3.js
+++ scripts/script_dataviz3.js
@@ -1,87 +1,34 @@
-d3.dsv(';', 'data/147_desratizacion.csv', d3.autoType).then(data => {
-  const mataderosData = data.filter(d => d.domicilio_barrio === "MATADEROS");
-  const palermoData = data.filter(d => d.domicilio_barrio === "PALERMO");
 
-  const contactos_cerrados_mataderos = mataderosData.filter(d => d.estado_del_contacto === "Cerrado").length;
-  const contactos_abiertos_mataderos = mataderosData.filter(d => d.estado_del_contacto === "Abierto").length;
+d3.dsv(';', 'data/contactos_anuales.csv', d3.autoType).then(data => {
+  const marzoAbril = data.filter(d => {
+    const fecha = d.fecha_ingreso.split('/');
+    const categoria = d.categoria;
+    const mes = parseInt(fecha[1]);
+    const barrio = d.domicilio_barrio
+    return categoria === "LIMPIEZA Y RECOLECCIÓN" && (barrio === 'CABALLITO' || barrio === 'PALERMO') && (mes === 03 || mes === 04);
+  });
 
-  const contactos_cerrados_palermo = palermoData.filter(d => d.estado_del_contacto === "Cerrado").length;
-  const contactos_abiertos_palermo = palermoData.filter(d => d.estado_del_contacto === "Abierto").length;
+  
 
-  let chart = Plot.plot({
-    height: 440,
-    width: 1000,
-    nice: true,
-
-    x: {
-      label: "",
-      tickFormat: null,
-      tickSize: 0,
-      ticks: 0,
-      domain: [-0.6, 0.6], // Ajuste del dominio para permitir espacio adicional
-    },
-    y: {
-      ticks: 0,
-    },
-    r: {
-      range: [0, 170],
-    },
+  const chartMarzoAbril = Plot.plot({
+    marginLeft: 60,
+    marginRight: 60,
+    grid: true,
+    x: { label: null },
+    fx: { label: null },
+    y: { domain: [0, 2600] },
+    color: { legend: true },
     marks: [
-      Plot.dot([{ tipo: "Cerrados", cantidad: contactos_cerrados_mataderos }], {
-        x: -0.45,
-        y: 0.5,
-        r: 'cantidad',
-        fill: '#225EA8'
-      }),
-      Plot.dot([{ tipo: "Abiertos", cantidad: contactos_abiertos_mataderos }], {
-        x: -0.2,
-        y: 0.5,
-        r: 'cantidad',
-        fill: '#C7D9F1'
-      }),
-      Plot.text([{ tipo: "Cerrados", cantidad: contactos_cerrados_mataderos }], {
-        x: -0.45,
-        y: 0.5,
-        text: (d) => ((d.cantidad).toFixed(0) + " cerrados"),
-        fill: "white",
-        fontSize: 25,
-      }),
-      Plot.text([{ tipo: "Abiertos", cantidad: contactos_abiertos_mataderos }], {
-        x: -0.2,
-        y: 0.5,
-        text: (d) => ((d.cantidad).toFixed(0) + " abiertos"),
-        fill: "white",
-        fontSize: 25,
-      }),
+      Plot.barY(marzoAbril,
+        Plot.groupX({ y: 'count' },
+          {  fx:'domicilio_barrio',x: 'estado_del_contacto', fill: (d) => d.estado_del_contacto === 'Abierto' ? '#225EA8' : '#93B8E0' })
+      ),
+      Plot.frame()
+    ]
+  });
 
-      Plot.dot([{ tipo: "Cerrados", cantidad: contactos_cerrados_palermo }], {
-        x: 0.17,
-        y: 0.5,
-        r: 'cantidad',
-        fill: '#2CA25F'
-      }),
-      Plot.dot([{ tipo: "Abiertos", cantidad: contactos_abiertos_palermo }], {
-        x: 0.42,
-        y: 0.5,
-        r: 'cantidad',
-        fill: '#C7E9B4'
-      }),
-      Plot.text([{ tipo: "Cerrados", cantidad: contactos_cerrados_palermo }], {
-        x: 0.163,
-        y: 0.5,
-        text: (d) => ((d.cantidad).toFixed(0) + " cerrados"),
-        fill: "white",
-        fontSize: 15,
-        }),
-        Plot.text([{ tipo: "Abiertos", cantidad: contactos_abiertos_palermo }], {
-        x: 0.42,
-        y: 0.5,
-        text: (d) => ((d.cantidad).toFixed(0) + " abiertos"),
-        fill: "white",
-        fontSize: 25,
-        }),
-        ],
-        });
-        
-        d3.select('#chart_def_3').append(() => chart);
-      });
+
+  d3.select('#chart_def_3').append(() => chartMarzoAbril);
+
+});
+
